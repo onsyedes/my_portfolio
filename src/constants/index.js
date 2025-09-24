fcc616183d87d6f89f1e4609a2c6ce63f0fac0d4
onsyedes <ons.yedes@tek-up.de>
2025-09-24 20:46:00 +0100

    --

---
+++ src/constants/index.js
@@ -0,0 +1,321 @@
+
+export const myProjects = [
+  {
+    id: 1,
+    title: "Navin General Agent",
+    description:
+      "AI-powered assistant engineered to help you strategize, streamline, and execute complex workflows across marketing, sales, HR, and finance.",
+    subDescription: [
+      "Development of authentication services with Supabase and user management systems.",
+      "Chatbot Interfacing.",
+      "Code review assurance" ],
+    href: "https://navin.live/",
+    logo: "/assets/projects/navin_gen/navin-logo.png",
+    image: "/assets/projects/navin_gen/hero.png",
+    tags: [
+      {
+        id: 1,
+        name: "Typescript",
+        path: "/assets/logos/typescript-logo.png",
+      },
+       {
+        id: 2,
+        name: "Next.js",
+        path: "/assets/logos/nextjs.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "Supabase",
+        path: "/assets/logos/supabase.svg",
+        style: "size-20 -translate-y-4",
+      },
+      
+    ],
+  },
+  {
+    id: 2,
+    title: "RAGDL Platform",
+    description:
+      "RAGDL is an advanced, modular AI platform built for secure, scalable, and intelligent data orchestration.",
+    subDescription: [
+      "Design and development of backend micro-services with the FastAPI framework.",
+      "Integration of pre-developed APIs by other application bricks (OpenAI, Google Cloud Firestore...).",
+      "Built interactive interfaces using Next.js and React Flow to design and manage component connections and workflows.",
+      
+    ],
+    href: "https://ragdl.ai/",
+    logo: "",
+    image: "/assets/projects/ragdl/ragdl.png",
+    tags: [
+      {
+        id: 1,
+        name: "FastApi",
+        path: "/assets/logos/fastAPI.svg",
+        style: "size-16"
+      },
+      {
+        id: 2,
+        name: "Python",
+        path: "/assets/logos/python-logo.png",
+        style: "w-20 h-12",
+      },
+      {
+        id: 3,
+        name: "SqLite",
+        path: "/assets/logos/sqlite.svg",
+        style: "w-20 h-12",
+      },
+      {
+        id: 4,
+        name: "Next.JS",
+        path: "/assets/logos/nextjs.svg",
+      },
+      {
+        id: 5,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 6,
+        name: "React Flow",
+        path: "/assets/logos/react-flow.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Stock Management System",
+    description:
+      "A modern, interactive MERN Stack web application for a factory, allowing inventory and suppliers to be managed in real time",
+    subDescription: [
+      "Creation of interactive tables for inventory monitoring",
+      "Implementation of an automatic notification system",
+      "Real-time user and data management",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/mam_pack/ajout-preforme.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDb",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/expressJs.png",
+        style: 'w-20 h-8 translate-y-1',
+      },
+      {
+        id: 3,
+        name: "React Js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "Node Js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 5,
+        name: "React SWR",
+        path: "/assets/logos/swr.png",
+      },
+      {
+        id: 6,
+        name: "Redux Toolkit",
+        path: "/assets/logos/redux.svg",
+      },
+      {
+        id: 7,
+        name: "NginX",
+        path: "/assets/logos/nginx.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Digital Ambassadors",
+    description:
+      "A platform that automatically collects and analyzes the LinkedIn activities of JEMS Group employees",
+    subDescription: [
+      "Development of a scraping algorithm with weekly cron jobs using Puppeteer.",
+      "Analysis of data collected on LinkedIn activities with MongoDB and Node.js.",
+      "Displaying statistics as graphs with Chart.js",
+     ],
+    href: "https://github.com/onsyedes/JemsIn_backend/tree/main",
+    logo: "",
+    image: "/assets/projects/jemsIn/employee_interactions.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDb",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/expressJs.png",
+        style: 'w-20 h-8 translate-y-1',
+      },
+      {
+        id: 3,
+        name: "React Js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "Node Js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      
+      {
+        id: 6,
+        name: "Redux Toolkit",
+        path: "/assets/logos/redux.svg",
+      },
+      
+      {
+        id: 7,
+        name: "Puppeteer",
+        path: "/assets/logos/puppeteer.png",
+      },
+      {
+        id: 7,
+        name: "RabbitMQ",
+        path: "/assets/logos/rabbitmq.png",
+      },
+
+    ],
+  },
+
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/+21651914302",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ons-yedes-a75888209/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  
+];
+
+export const experiences = [
+  {
+    title: "Full-stack JavaScript Developer",
+    site: 'https://navinspire.ai/',
+    // job: "Navinspire AI",
+    date: "April 2024 – Present",
+    logo: '/assets/logos/navinspire.png',
+    contents: [
+      "Designed an AI assistant to automate workflows across marketing, HR, and finance departments.",
+      "Developed authentication services with Supabase and implemented user management systems.",
+      "Integrated chatbot solutions and conducted code reviews.",
+      "Built a modular, AI-driven data platform (Rag Data Lake) to centralize and process multi-source data.",
+      "Developed backend microservices using FastAPI.",
+      "Integrated APIs from external services (OpenAI, Firestore, etc.).",
+      "Created visual interfaces with React Flow to connect components and manage workflows in a Next.js app."
+    ]
+  },
+  {
+    title: "Full-stack JavaScript Developer (Freelance)",
+    // job: "MaM Pack",
+    logo:'/assets/logos/mam_pack.png',
+    site: 'https://mampack.com/',
+    date: "Sep. 2023 – Feb. 2024",
+    contents: [
+      "Developed a web platform for factory inventory and supplier management in real time.",
+      "Built interactive tables for monitoring stock.",
+      "Implemented an automatic notification system.",
+      "Enabled real-time user and data management."
+    ]
+  },
+  {
+    title: "End-of-studies Intern",
+    logo: "/assets/logos/jems.svg",
+    style: 'size-14',
+    site: "https://www.linkedin.com/company/jems-group/posts/?feedView=all",
+    date: "Feb. 2023 – July 2023",
+    contents: [
+      "Developed a platform to collect and analyze LinkedIn activity of company employees.",
+      "Created a scraping algorithm with weekly cron jobs.",
+      "Analyzed collected LinkedIn data to extract insights.",
+      "Displayed statistics through dynamic graphs."
+    ]
+  },
+  {
+    title: "Frontend Developer (Freelance)",
+    logo: "/assets/logos/ijeni.svg",
+    style: 'size-16',
+    site: "https://ijeni.tn/",
+    date: "Oct. 2022 – Nov. 2022",
+    contents: [
+      "Built a multi-service web platform with Angular to connect local providers and customers.",
+      "Developed responsive user interfaces.",
+      "Facilitated communication and coordination between service providers and users."
+    ]
+  }
+]
+
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
